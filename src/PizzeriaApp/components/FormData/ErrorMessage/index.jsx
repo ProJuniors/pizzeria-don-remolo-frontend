921ac972fb2592ea18eae83dc1b5371b8b9fa1f4
Gary <gary.yaral@gmail.com>
2022-11-04 08:31:53 -0500

    vercel.json was added

--- src/PizzeriaApp/components/FormData/ErrorMessage/index.jsx
+++ src/PizzeriaApp/components/FormData/ErrorMessage/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { errorsMessage } from '../../../../utils/validateFields.utils';
+
 export default function ErrorMessage ({field}) {
   return (
     <small className="px-[10px] width-full text-red-500 text-[1rem]">{errorsMessage[field]}</small>
